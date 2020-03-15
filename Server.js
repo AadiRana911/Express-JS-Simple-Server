f0adea838fd9839416ca60ec567a6efc6ca28297
AsadImtiazRana <asadimtiaz711@gmail.com>
2020-03-15 18:45:29 +0500

    Get and Post methods with express JS

--- Server.js
+++ Server.js
@@ -3,6 +3,16 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('<h1>Hello</h1>');
+    res.send('<h1>Getting Root</h1>');
+})
+app.get('/profile', (req, res) => {
+    res.send('<h1>Getting Profile</h1>');
+})
+app.post('/profile', (req, res) => {
+    const user = {
+        name: 'Asad',
+        hobby: 'Gaming'
+    }
+    res.send(`<h1>${user}</h1>`);
 })
 app.listen(3000);
